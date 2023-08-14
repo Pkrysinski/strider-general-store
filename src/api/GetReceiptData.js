5fd42467ca5ae3d687f3fc372e73571f2e2c1a64
Peter Krysinski <pakrysin@gmail.com>
2023-08-14 12:30:33 -0600

    Adding the dependencies inside of the useEffect hook so that the Railway build can deploy successfully

--- src/api/GetReceiptData.js
+++ src/api/GetReceiptData.js
@@ -2,33 +2,32 @@ import { useEffect, useState } from "react";
 
 function GetReceiptData() {
 
-    let requestOptions = {
-        method: 'GET',
-        headers : {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-    };
-
-    let requestUrl = 'https://doc.strider.tech/content/receipts.json';
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-
     const [receipts, setReceipts] = useState([]);
 
     useEffect(() => {
+        const fetchData = async (response) => {
+            let requestOptions = {
+                method: 'GET',
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+            };
 
-    const fetchData = async (response) => {
-        try {
-            const response = await fetch(proxyUrl + requestUrl,requestOptions);
-            const results = await response.json();
-            // Only put the results in state, ie, the actual receipts array
-            return setReceipts(results);
-        } catch (error) {
-            console.log("Error: ", error);
-        }
-    };
+            let requestUrl = 'https://doc.strider.tech/content/receipts.json';
+            let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
-    fetchData();
+            try {
+                const response = await fetch(proxyUrl + requestUrl,requestOptions);
+                const results = await response.json();
+                // Only put the results in state, ie, the actual receipts array
+                return setReceipts(results);
+            } catch (error) {
+                console.log("Error: ", error);
+            }
+        };
+
+        fetchData();
     },[]);
 
     return receipts;
